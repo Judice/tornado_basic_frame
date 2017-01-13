778ba6eba34ed78afa449c9dff955a6d162c3e70
acezio.han <bolide2005@163.com>
2017-01-13 15:20:01 +0800

    correct the handler routes defination

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,5 +3,5 @@
 import index_handler
 import async_handler
 
-ROUTES = [(r'/', index_handler.IndexHandler),]
-ROUTES = [(r'/async', async_handler.AsyncHandler),]
+ROUTES = [(r'/index', index_handler.IndexHandler),
+          (r'/async', async_handler.AsyncHandler),]
